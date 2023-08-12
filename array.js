dfbacc08ab519096f7f86ed1af2ee25e19abb9f4
KimJinkYU <jinkyu1996@hanmail.net>
2023-08-12 15:31:02 +0900

    Add: array.js

---
+++ array.js
@@ -0,0 +1,15 @@
+let arr1 = []; // 배열 리터럴
+let arr2 = new Array();
+
+// 다양한 항목들을 배열로 추가할 수 있다.
+let arr3 = [1, '2', true, null, undefined, {}, [], function () {}];
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+
+arr.push(6);
+console.log(arr);
+console.log(arr.length);
